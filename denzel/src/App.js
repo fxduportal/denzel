adc9e182ceb850fd8b6b6d378ea48f6dae1c1785
fxduportal <fxduportal@outlook.fr>
2020-04-07 23:09:12 +0000

    tab + api deployement

--- denzel/src/App.js
+++ denzel/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
+import Tab from './tab';
 
 function App() {
-  return (
-    <div className="App">
-    <h3>Here are the best Denzel Films</h3>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Tab/>
+        </div>
+    );
 }
 
 export default App;
---
+++ denzel/src/tab.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Tabs, Tab, Panel } from '@bumaga/tabs' 
+
+export default () => (
+  <Tabs>
+    <div>
+      <Tab><button>Tab 1</button></Tab>
+      <Tab><button>Tab 2</button></Tab>
+      <Tab><button>Tab 3</button></Tab>
+    </div>
+
+    <Panel><p>Panel 1</p></Panel>
+    <Panel><p>Panel 2</p></Panel>
+    <Panel><p>panel 3</p></Panel>
+  </Tabs>
+);
