304d7d7f7b76adda5e9b8fee14c67c920321b0b0
fxduportal <fxduportal@outlook.fr>
2020-04-08 13:50:32 +0000

    test

--- server/index.js
+++ server/index.js
@@ -67,9 +67,14 @@ app.use('/.netlify/functions/index', router);
 
 router.options('*', cors());
 
+
+router.get('/',(req, res)=>{
+    res.send("Hello");
+});
 /**
  * Gets us all the movies, with a get request and an collection.find
  */
+
 router.get('/movies', async (request, response) => {
     let data = {};
     const client = await MongoClient.connect(CONNECTION_URL, { useUnifiedTopology: true, useNewUrlParser: true })
@@ -92,129 +97,130 @@ router.get('/movies', async (request, response) => {
         return data.data;
     }
     return 'empty';
+    response.send(data);
 });
 
-/**
- * Gets us a movie by its id
- */
-router.get('/moviesId/:id', async (request, response) => {
-    let data = {};
-    const client = await MongoClient.connect(CONNECTION_URL, { useUnifiedTopology: true, useNewUrlParser: true })
-        .catch((err) => {
-            console.log(err);
-        });
-    if (!client) {
-        return;
-    }
-    try {
-        const db = client.db(DATABASE_NAME);
-        const collectionMovie = db.collection('movie');
-        data = await collectionMovie.findOne({ 'movie.id': request.params.id }).toArray();
-    } catch (err) {
-        console.log(err);
-    } finally {
-        client.close();
-    }
-    if (data && data.data) {
-        return data.data;
-    }
-    return 'empty';
-});
+// /**
+//  * Gets us a movie by its id
+//  */
+// router.get('/moviesId/:id', async (request, response) => {
+//     let data = {};
+//     const client = await MongoClient.connect(CONNECTION_URL, { useUnifiedTopology: true, useNewUrlParser: true })
+//         .catch((err) => {
+//             console.log(err);
+//         });
+//     if (!client) {
+//         return;
+//     }
+//     try {
+//         const db = client.db(DATABASE_NAME);
+//         const collectionMovie = db.collection('movie');
+//         data = await collectionMovie.findOne({ 'movie.id': request.params.id }).toArray();
+//     } catch (err) {
+//         console.log(err);
+//     } finally {
+//         client.close();
+//     }
+//     if (data && data.data) {
+//         return data.data;
+//     }
+//     return 'empty';
+// });
 
-/**
- * Gets us a movie by its title
- */
-router.get('/moviesTitle/:title', async (request, response) => {
-      let data = {};
-    const client = await MongoClient.connect(CONNECTION_URL, { useUnifiedTopology: true, useNewUrlParser: true })
-        .catch((err) => {
-            console.log(err);
-        });
-    if (!client) {
-        return;
-    }
-    try {
-        const db = client.db(DATABASE_NAME);
-        const collectionMovie = db.collection('movie');
-        data = await collectionMovie.findOne({ 'title': request.params.id }).toArray();
-    } catch (err) {
-        console.log(err);
-    } finally {
-        client.close();
-    }
-    if (data && data.data) {
-        return data.data;
-    }
-    return 'empty';
-});
+// /**
+//  * Gets us a movie by its title
+//  */
+// router.get('/moviesTitle/:title', async (request, response) => {
+//       let data = {};
+//     const client = await MongoClient.connect(CONNECTION_URL, { useUnifiedTopology: true, useNewUrlParser: true })
+//         .catch((err) => {
+//             console.log(err);
+//         });
+//     if (!client) {
+//         return;
+//     }
+//     try {
+//         const db = client.db(DATABASE_NAME);
+//         const collectionMovie = db.collection('movie');
+//         data = await collectionMovie.findOne({ 'title': request.params.id }).toArray();
+//     } catch (err) {
+//         console.log(err);
+//     } finally {
+//         client.close();
+//     }
+//     if (data && data.data) {
+//         return data.data;
+//     }
+//     return 'empty';
+// });
 
-/**
- * Inserts the asked movie inside the collection movie of the db
- */
-router.post('/movie', (request, response) => {
-    collectionMovie.insertOne(request.body, (error, result) => {
-        if (error) {
-            error.reject();
-        }
-        response.send(result);
-    });
-});
+// /**
+//  * Inserts the asked movie inside the collection movie of the db
+//  */
+// router.post('/movie', (request, response) => {
+//     collectionMovie.insertOne(request.body, (error, result) => {
+//         if (error) {
+//             error.reject();
+//         }
+//         response.send(result);
+//     });
+// });
 
-/**
- * Inserts the asked movie inside the collection awesome of the db
- */
-router.post('/movie/aw', (request, response) => {
-    collectionAwesome.insertOne(request.body, (error, result) => {
-        if (error) {
-            error.reject();
-        }
-        response.send(result);
-    });
-});
+// /**
+//  * Inserts the asked movie inside the collection awesome of the db
+//  */
+// router.post('/movie/aw', (request, response) => {
+//     collectionAwesome.insertOne(request.body, (error, result) => {
+//         if (error) {
+//             error.reject();
+//         }
+//         response.send(result);
+//     });
+// });
 
-app.listen(PORT);
-console.log(`📡 Running on port ${PORT}`);
-//#endregion
+// app.listen(PORT);
+// console.log(`📡 Running on port ${PORT}`);
+// //#endregion
 
-//#region intiDB
-const init = require('./init');
-const axios = require('axios');
+// //#region intiDB
+// const init = require('./init');
+// const axios = require('axios');
 
-/**
- * Function that clean our database, gets all the data from imdb and fill up the database from the collected data
- */
-let initDb = async () => {
-    let { movies, awesome } = await init.start();
-    let counterAwesome = 0, counterMovies = 0;
+// /**
+//  * Function that clean our database, gets all the data from imdb and fill up the database from the collected data
+//  */
+// let initDb = async () => {
+//     let { movies, awesome } = await init.start();
+//     let counterAwesome = 0, counterMovies = 0;
 
-    collectionMovie.deleteMany({}).then(async () => {
-        console.log('🗑 Previous movies database cleaned');
-        movies.forEach(movie => {
-            try {
-                axios.post(baseUrl + '/movie', { movie });
-                counterMovies += 1;
-            } catch (error) {
-                console.error(error);
-            }
-        });
-        console.log(`🎬 ${counterMovies} movies added to the database`);
-    }, (error) => {
-        console.error(error);
-    });
+//     collectionMovie.deleteMany({}).then(async () => {
+//         console.log('🗑 Previous movies database cleaned');
+//         movies.forEach(movie => {
+//             try {
+//                 axios.post(baseUrl + '/movie', { movie });
+//                 counterMovies += 1;
+//             } catch (error) {
+//                 console.error(error);
+//             }
+//         });
+//         console.log(`🎬 ${counterMovies} movies added to the database`);
+//     }, (error) => {
+//         console.error(error);
+//     });
 
-    collectionAwesome.deleteMany({}).then(async () => {
-        console.log('🗑 Previous awesome database cleaned');
-        awesome.forEach(movie => {
-            try {
-                axios.post(`${baseUrl}/movie/aw`, { movie });
-                counterAwesome += 1;
-            } catch (error) {
-                console.error(error);
-            }
-        });
-        console.log(`🌟 ${counterAwesome} awesome movies registered in the database`);
-    });
-};
+//     collectionAwesome.deleteMany({}).then(async () => {
+//         console.log('🗑 Previous awesome database cleaned');
+//         awesome.forEach(movie => {
+//             try {
+//                 axios.post(`${baseUrl}/movie/aw`, { movie });
+//                 counterAwesome += 1;
+//             } catch (error) {
+//                 console.error(error);
+//             }
+//         });
+//         console.log(`🌟 ${counterAwesome} awesome movies registered in the database`);
+//     });
+// };
 
-//initDb();
-//#endregion
+// //initDb();
+// //#endregion
